5eac3e4177fffea483b212d2b1d6830f74f04a47
DeathKira <4E657F6G@users.noreply.github.com>
2018-04-25 17:59:40 -0600

    last

--- Pong/main.py
+++ Pong/main.py
@@ -7,6 +7,19 @@ from kivy.clock import Clock
 from random import randint
 
 
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            vx = ball.velocity
+            vy = ball.velocity
+            offset = (ball.center_x - self.center_x) / (self.width / 2)
+            bounced = Vector(-1 * vx, vy)
+            vel = bounced * 1.1
+            ball.velocity = vel.y, vel.x + offset
+
+
 class PongBall(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
@@ -19,9 +32,10 @@ class PongBall(Widget):
 class PongGame(Widget):
     ball = ObjectProperty(None)
 
-    def serve_ball(self):
+    def serve_ball(self, vel=(4, 0)):
         self.ball.center = self.center
-        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+        # self.ball.velocity = vel.rotate(randint(0, 360))
+        self.ball.velocity = vel
 
     def update(self, dt):
         self.ball.move()
@@ -30,23 +44,27 @@ class PongGame(Widget):
         self.player1.bounce_ball(self.ball)
         self.player2.bounce_ball(self.ball)
 
-        # bounce ball off bottom or top
-        if (self.ball.y < self.y) or (self.ball.top > self.top):
+        # bounce off top and bottom
+        if (self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.velocity_y *= -1
 
+        # bounce off left and right
+        if (self.ball.x < 0) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
+
         # went of to a side to score point?
-        if self.ball.x < self.x:
+        if self.ball.y < self.y:
             self.player2.score += 1
             self.serve_ball(vel=(4, 0))
-        if self.ball.x > self.width:
+        if self.ball.y > self.height:
             self.player1.score += 1
             self.serve_ball(vel=(-4, 0))
 
     def on_touch_move(self, touch):
-        if touch.x < self.width / 3:
-            self.player1.center_y = touch.y
-        if touch.x > self.width - self.width / 3:
-            self.player2.center_y = touch.y
+        if touch.y < self.height / 3:
+            self.player1.center_x = touch.x
+        if touch.y > self.height - self.height / 3:
+            self.player2.center_x = touch.x
 
 
 class PongApp(App):
